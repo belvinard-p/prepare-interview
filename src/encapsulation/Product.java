5fd7802ba7804f71b078eaa24966a2279b7ed938
Belvinard Pouadjeu <pouadjeubelvi@gmail.com>
2025-10-21 13:48:27 +0000

    Add constructor to Product class
    Added a constructor to initialize product attributes.

--- src/encapsulation/Product.java
+++ src/encapsulation/Product.java
@@ -3,6 +3,12 @@ public class Product {
   private double price;
   private int quantity;
 
+  public Product(String name, double price, int quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
   public void setProductName(String name){
   }
 }
