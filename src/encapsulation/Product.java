87de163e9182d8576dedfbb0b5dec9918248db4b
belvip <pouadjeubelvi@gmail.com>
2025-10-21 13:45:09 +0000

    add setter and getter

--- src/encapsulation/Product.java
+++ src/encapsulation/Product.java
@@ -1,8 +1,18 @@
+package encapsulation;
+
 public class Product {
   private String name;
   private double price;
   private int quantity;
 
   public void setProductName(String name){
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public int getQuantity() {
+    return quantity;
   }
 }
